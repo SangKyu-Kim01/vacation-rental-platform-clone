98a7c5b31713fcb491aa5f2359a63a6022e0198b
SangKyuKim00 <sangkyusky@gmail.com>
2024-08-13 18:05:19 -0400

    Reorganized routes for improved page loading performance and correct layout application.

--- frontend/src/routes/Routers.js
+++ frontend/src/routes/Routers.js
@@ -24,136 +24,38 @@ import Review from "../pages/Review";
 function Routers() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
-      <Route
-        path="/home"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
+      {/* Route lists that do not need to be wrapped in Layout */}
       <Route path="/auth-callback" element={<AuthCallBack />} />
-      <Route
-        path="/search"
-        element={
-          <Layout>
-            <SearchPage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/listings/:id"
-        element={
-          <Layout>
-            <ListingDetails />
-          </Layout>
-        }
-      />
       <Route path="/listings/gallery/:id" element={<Gallery />} />
+
+      {/* Route lists that need to be wrapped in Layout */}
       <Route
-        path="/account/listings"
         element={
           <Layout>
-            <UserListing />
+            <Outlet /> {/* Outlet is used to render nested routes */}
           </Layout>
         }
-      />
-      <Route
-        path="/account/listing/new"
-        element={
-          <Layout>
-            <CreateListing />
-          </Layout>
-        }
-      />
-      <Route
-        path="/account/listing/update/:id"
-        element={
-          <Layout>
-            <UpdateListing />
-          </Layout>
-        }
-      />
-      <Route
-        path="/account"
-        element={
-          <Layout>
-            <Profile />
-          </Layout>
-        }
-      />
-      <Route
-        path="/account/bookings"
-        element={
-          <Layout>
-            <GuestBookings />
-          </Layout>
-        }
-      />
-      <Route
-        path="/account/messages"
-        element={
-          <Layout>
-            <UserMessage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/booking"
-        element={
-          <Layout>
-            <Booking />
-          </Layout>
-        }
-      />
-      <Route
-        path="/contact_host/:id"
-        element={
-          <Layout>
-            <ContactHost />
-          </Layout>
-        }
-      />
-      <Route
-        path="/payment-details"
-        element={
-          <Layout>
-            <PaymentDetails />
-          </Layout>
-        }
-      />
-      <Route
-        path="/payment-canceled"
-        element={
-          <Layout>
-            <PaymentCancel />
-          </Layout>
-        }
-      />
-      <Route
-        path="/admin"
-        element={
-          <Layout>
-            <Admin />
-          </Layout>
-        }
-      />
-      <Route
-        path="/account/review"
-        element={
-          <Layout>
-            <Review />
-          </Layout>
-        }
-      />
+      >
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/listings/:id" element={<ListingDetails />} />
+
+        {/* Account-related routes */}
+        <Route path="/account/listings" element={<UserListing />} />
+        <Route path="/account/listing/new" element={<CreateListing />} />
+        <Route path="/account/listing/update/:id" element={<UpdateListing />} />
+        <Route path="/account/bookings" element={<GuestBookings />} />
+        <Route path="/account/messages" element={<UserMessage />} />
+        <Route path="/account/review" element={<Review />} />
+        <Route path="/account" element={<Profile />} />
+
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/contact_host/:id" element={<ContactHost />} />
+        <Route path="/payment-details" element={<PaymentDetails />} />
+        <Route path="/payment-canceled" element={<PaymentCancel />} />
+        <Route path="/admin" element={<Admin />} />
+      </Route>
     </Routes>
   );
 }
